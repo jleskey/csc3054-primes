6d4eb99f64efc44783eb596dd60fd5410d966e23
Joseph Leskey <joseph@josephleskey.com>
2024-10-25 22:41:52 -0400

    initialize repository

---
+++ sources/sum_primes.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+void printPrimes(const int count);
+int sumPrimes(const int count);
+int getUpperBound(const int count);
+int* generatePrimes(const int count);
+
+int main(int argc, char** argv) {
+    if (argc == 2) {
+        const int count = atoi(argv[1]);
+        printf("%d\n", sumPrimes(count));
+    } else {
+        printf("Proper usage: %s <number of primes>\n", argv[0]);
+        return 1;
+    }
+}
+
+void printPrimes(const int count) {
+    int* primes = generatePrimes(count);
+    for (size_t i = 0; i < count; i++) {
+        printf("%d%s", primes[i], i == count - 1 ? "\n" : ", ");
+    }
+    free(primes);
+}
+
+int sumPrimes(const int count) {
+    int* primes = generatePrimes(count);
+
+    int sum = 0;
+
+    for (size_t i = 0; i < count; i++) {
+        sum += primes[i];
+    }
+
+    free(primes);
+
+    return sum;
+}
+
+int* generatePrimes(const int count) {
+    const double upper_bound = getUpperBound(count);
+
+    int sum = 0;
+
+    bool* sieve = malloc(sizeof(bool) * (upper_bound + 1));
+
+    // We'll just hard code 0 and 1 to being non-prime. We're not using
+    // them.
+    sieve[0] = sieve[1] = false;
+
+    // We'll start by assuming all numbers from 2 through the upper bound
+    // are prime. It's probably an incorrect assumption, but you have to
+    // start somewhere.
+    for (size_t i = 2; i <= upper_bound; i++) {
+        sieve[i] = true;
+    }
+
+    // As it turns out, multiples of numbers greater than one are generally
+    // not primes, so let's reflect that.
+    for (size_t i = 2; i <= sqrt(upper_bound); i++) {
+        if (sieve[i]) {
+            for (size_t j = i * i; j <= upper_bound; j += i) {
+                sieve[j] = false;
+            }
+        }
+    }
+
+    // The numbers have been sifted to bring out the primes. Now let's count
+    // out the correct number.
+    int* prime_list = malloc(sizeof(int) * count);
+    for (size_t bi = 2, ii = 0; bi <= upper_bound && ii < count; bi++) {
+        if (sieve[bi]) {
+            prime_list[ii] = bi;
+            ii++;
+        }
+    }
+
+    // The sieve has served its purpose. May it rest in peace.
+    free(sieve);
+
+    return prime_list;
+}
+
+int getUpperBound(const int count) {
+    return count < 6
+        ? (int[]){0, 2, 3, 5, 7, 11}[count]
+        : count * (log(count) + log(log(count)));
+}
