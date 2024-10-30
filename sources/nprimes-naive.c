108b9497620c105b28346836bc829d905e93c3bf
Joseph Leskey <joseph@josephleskey.com>
2024-10-30 03:20:51 -0400

    Add missing logic

--- sources/nprimes-naive.c
+++ sources/nprimes-naive.c
@@ -18,6 +18,8 @@ int main() {
 }
 
 int isPrime(int x) {
+    // By convention, we consider only positive prime elements (numbers).
+    if (x < 2) return false;
     for (int i = 2; i <= sqrt(x); i++)
         if (x % i == 0)
             return false;
