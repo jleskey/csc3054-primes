0edef9c7f316f1e82a16170f964e8300f2b27ff0
Joseph Leskey <joseph@josephleskey.com>
2024-11-01 00:56:31 -0400

    Fix store word attempt

--- assembly/counter.s
+++ assembly/counter.s
@@ -9,10 +9,11 @@ countdown:
 main:
     lw      t0, countup
     lw      t1, countdown
+    la      t2, countup
 loop:
     addi    t0, t0, 1
     addi    t1, t1, -1
-    sw      t0, countup
+    sw      t0, 0(t2)
     bnez    t1, loop
 exit:
     li      a0, 0
